dccfaee01afdf2c3e1adddab4708b6e3ca0e35e8
vintexlens <vintexlens@gmail.com>
2023-02-16 13:46:38 -0800

    5-using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * program that prints using printf
+ * the program returns zero value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
