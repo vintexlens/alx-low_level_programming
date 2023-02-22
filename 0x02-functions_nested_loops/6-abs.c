69b1528e09a0c74518f2864d4b03ede2f96398e2
vintexlens <vintexlens@gmail.com>
2023-02-22 05:19:41 -0800

    6th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer.
+ * @x: the integer to check
+ * Return: 0
+ */
+
+int _abs(int x)
+{
+	int result;
+
+	if (x < 0)
+	{
+		result = (x * (-1));
+		return (result);
+	}
+	else
+		result = x;
+}
