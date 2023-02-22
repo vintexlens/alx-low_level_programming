3f94f447c11b10d9a28a213ec29ed52ef3598c6a
vintexlens <vintexlens@gmail.com>
2023-02-21 17:10:11 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * main - writes a program that prints _putchar
+ * main - printing putchar
  *
- * Return: On success 0.
+ * Return: 0
  */
+
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 	return (0);
 }
