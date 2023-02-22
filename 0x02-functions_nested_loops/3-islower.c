f392b77620eca8497c3e4202c3a5377a1d984002
vintexlens <vintexlens@gmail.com>
2023-02-22 03:59:32 -0800

    task 03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @c: varriable that is passed
+ * return: 1 if c is lowercase
+ */
+
+int _islower(int c)
+{
+	while (c == islower)
+	{
+		_putchar(1);
+	}
+	else
+		_putchar(0);
+
+	return (_putchar);
+}
