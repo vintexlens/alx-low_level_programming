c599b672230897494a78a115019ce2c5831b920a
vintexlens <vintexlens@gmail.com>
2023-02-22 01:50:40 -0800

    task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ *
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	for (i = 0; i <= 10; i++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+}
