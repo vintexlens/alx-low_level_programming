d3547888aba490820cd0935d86b2dd056e855d56
vintexlens <vintexlens@gmail.com>
2023-02-22 05:02:28 -0800

    4th task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: variable number to be passed
+ *
+ * Return: 0, 1 or -1 on success
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);	
+		return (-1);
+	}
+}
+
