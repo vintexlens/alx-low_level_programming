ea02c950053d2b09c12b0f331d31b8933951599f
vintexlens <vintexlens@gmail.com>
2023-02-23 09:29:28 -0800

    4th task on more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		while (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+}
