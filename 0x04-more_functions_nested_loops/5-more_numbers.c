e92f0d7ffee777e2a0cf3c10543d794739cb810c
vintexlens <vintexlens@gmail.com>
2023-02-23 10:34:39 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 48; j < 63; j++)
+		{
+			_putchar (j);
+		}
+	_putchar ('\n');
+	}
+}
