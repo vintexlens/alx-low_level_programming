cda13e2b9195f5ad075ba1e831aabb2afea4b341
vintexlens <vintexlens@gmail.com>
2023-02-23 06:34:22 -0800

    task 2 on more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: first varriable
+ * @b: second varriable
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+
+	return (result);
+}
