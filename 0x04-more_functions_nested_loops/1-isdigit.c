36f772ef9ab53aed4bb4e2813e1bc73055390f1c
vintexlens <vintexlens@gmail.com>
2023-02-23 06:17:05 -0800

    task 1 on more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit
+ * @c: our varriable to be passed
+ *
+ * Return: 1 if c is a digit else 0
+ */
+
+int _isdigit(int c)
+{
+	while (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	return (0);
+}
