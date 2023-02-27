f87a713a1231f8324ac7368ba14ba155fe2879f0
vintexlens <vintexlens@gmail.com>
2023-02-27 12:07:10 -0800

    printing on reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse followed by a new line
+ * @s: pointer
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+	_putchar(s[i]);
+	i--;
+	}
+	_putchar('\n');
+}
