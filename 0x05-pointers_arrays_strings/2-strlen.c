8d2a70b7405ff88c1df38fe6b554625614b7bbe8
vintexlens <vintexlens@gmail.com>
2023-02-27 11:39:37 -0800

    checking string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,19 @@
 #include "main.h"
-#include <string.h>
 /**
  * _strlen - function that returns the length of a string.
  * @s: pointer
  *
- * Return: void
+ * Return: 0
  */
 
 int _strlen(char *s)
 {
-	strlen(s);
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+
 }
