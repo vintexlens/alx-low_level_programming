b1bf75d9bff58dce0f623dda73ff3817abedf28a
vintexlens <vintexlens@gmail.com>
2023-02-27 11:05:54 -0800

    finding string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
