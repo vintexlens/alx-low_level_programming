82eb6a96d1a039eeb4ff923a9cecfc01d6f62f33
vintexlens <vintexlens@gmail.com>
2023-02-27 15:35:50 -0800

    reversing strings 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,24 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int i, max, half;
+	char first, last;
+
+	i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (i >= 0)
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
 	{
-		_putchar(s[i]);
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
 	}
-	_putchar('\n');
 
 }
