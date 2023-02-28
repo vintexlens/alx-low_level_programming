41e523e1e169b936b77ffd6bad22371cf519a124
vintexlens <vintexlens@gmail.com>
2023-02-27 16:38:14 -0800

    printing out arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,12 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
-	
-	while (a[i] != '\0')
+	n = 0;
+
+	while (a[n] != '\0')
 	{
-		printf("%d, ", a[i]);
+		printf("%d, ", a[n]);
+		n++;
 	}
 	printf("\n");
 
