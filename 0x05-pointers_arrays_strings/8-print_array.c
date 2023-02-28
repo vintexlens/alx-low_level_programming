3300ae3181bbdbb4a59081add3c1b28735cd1ef1
vintexlens <vintexlens@gmail.com>
2023-02-27 16:33:23 -0800

    printing out arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of an array of integers
+ * @a: pointer
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	a[n];
+
+	while (a[i] != '\0')
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+
+}
