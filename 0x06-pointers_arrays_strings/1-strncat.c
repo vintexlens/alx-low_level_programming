4621be42f24e5fee0c996f239f8152504f37f798
vintexlens <vintexlens@gmail.com>
2023-03-02 06:39:46 -0800

    strncat vs strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	size_t dest_len = strlen(dest);
-	size_t i;
+	int i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-		dest[dest_len + i] = '\0';
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
