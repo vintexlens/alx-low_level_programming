ee809be956bcad7d0393f3181b8fdbc05a502928
vintexlens <vintexlens@gmail.com>
2023-03-02 07:11:22 -0800

    comparing sterings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: first pointer
+ * @s2: second pointer
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+
+	return (0);
+}
