dc3dffe6e1018cd81dacc02c98056c8378a52df7
vintexlens <vintexlens@gmail.com>
2023-03-02 12:45:55 -0800

    more pointers 01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int len;
 
-	for (len = 0; dest[len] != '\0'; len++)
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[len + i] = src[i];
-	dest[len + i] = '\0';
-	return (dest);
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
+
+/*should end with a end of string char*/
+dest[dest_len + i] = '\0';
+
+return (dest);
 }
