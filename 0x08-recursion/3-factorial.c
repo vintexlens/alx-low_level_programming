bb254ad2a7327c8c37abeddbf80c81d240cd8559
vintexlens <vintexlens@gmail.com>
2023-03-08 15:50:14 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: number for factorial
+ *
+ * Return: 0, the function should return -1 to indicate an error
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
