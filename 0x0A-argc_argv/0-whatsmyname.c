e009a148b37cc230e46033d55b50848c2e2906d6
vintexlens <vintexlens@gmail.com>
2023-03-10 18:12:19 -0800

    argc task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: command arguments
+ * @argv: argument size
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+argc vs argv
