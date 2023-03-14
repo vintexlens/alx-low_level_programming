0ca0641f2090772f584498a2b0f32669ec6926cb
vintexlens <vintexlens@gmail.com>
2023-03-14 16:34:22 -0700

    task 0 creating array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,9 +24,11 @@ char *create_array(unsigned int size, char c)
 		for (i = 0; i < size; i++)
 		{
 			*(arr + i) = c;
-			return (arr);
 		}
+		return (arr);
 	}
 	else
+	{
 		return (NULL);
+	}
 }
