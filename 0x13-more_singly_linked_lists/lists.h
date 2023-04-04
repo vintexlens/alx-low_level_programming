eea917874292d09982f841329269cf07826fe906
vintexlens <vintexlens@gmail.com>
2023-04-03 22:55:34 -0700

    add node at end

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,12 +5,23 @@
 #include <string.h>
 #include <assert.h>
 
-int _putchar(char c)
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
+int _putchar(char c);
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
