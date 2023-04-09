7c8188b9e0665dc87f69109e1cd8a5b8b573ff52
vintexlens <vintexlens@gmail.com>
2023-04-09 11:17:18 -0700

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b:pointer value.
+ * Return: 0 (Success)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0;
+	int i = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i])
+	{
+		if (b[i] == '1')
+		{
+			a = a << 1;
+			a = a | 1;
+		}
+		else if (b[i] == '0')
+		{
+			a = a << 1;
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (a);
+}
