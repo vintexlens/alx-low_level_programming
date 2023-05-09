cfbaf82c7bd013b6be4393790a55197fcd728344
vintexlens <vintexlens@gmail.com>
2023-05-08 19:45:59 -0700

    added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[j])
 			j++;
-		i = write(f, text_content, b);
+		i = write(f, text_content, j);
 		if (i != j)
 			return (-1);
 	}
